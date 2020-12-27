d8fff2724ad1bf24ad9ab3107c43ac8a89c2b0cd
Brian Ibbotson <brian@quinceleaf.dev>
2020-12-27 05:48:33 -0500

    config urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,27 @@
-"""config URL Configuration
+# ProductionSGR URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path("admin/doc/", include("django.contrib.admindocs.urls")),
+    path("admin/", admin.site.urls),
+    path("user/", include("django.contrib.auth.urls")),
+    # path("__debug__/", include(debug_toolbar.urls)),
+    path("", include("apps.core.urls", namespace="core")),
+    path("", include("apps.production.urls", namespace="production")),
+    path("", include("apps.materials.urls", namespace="materials")),
+    path("", include("apps.purchasing.urls", namespace="purchasing")),
+    path("", include("apps.sales.urls", namespace="sales")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "SG|R Production System Administration"
+admin.site.site_title = "SG|R Production System Administration"
+admin.site.index_title = "SG|R Production System Administration"
