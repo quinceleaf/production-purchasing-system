d7b572b3e3d447e1c45887178be5da74a1a4b5fc
Brian Ibbotson <brian@quinceleaf.dev>
2020-12-27 05:22:39 -0500

    Initial migrations

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,104 @@
+# ––– DJANGO IMPORTS
 from django.db import models
+from django.http import HttpResponse
 
-# Create your models here.
+
+# ––– PYTHON UTILITY IMPORTS
+import csv
+
+
+# –––THIRD-PARTY IMPORTS
+from simple_history.models import HistoricalRecords
+import ulid
+
+
+# ––– PROJECT IMPORTS
+
+
+# ––– PARAMETERS
+
+
+# ––– MODELS
+
+
+def generate_ulid():
+    return str(ulid.ULID())
+
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment; filename={}.csv".format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+
+class HistoryMixin(models.Model):
+    history = HistoricalRecords(inherit=True)
+
+    def get_history(self):
+        return_data = []
+        all_histories = self.history.all()
+        for history in all_histories:
+            delta = history.diff_against(history.prev_record)
+            for change in delta.changes:
+                if change.old:
+                    comment = (
+                        f"{change.field} changed from {change.old} to {change.new}"
+                    )
+                else:
+                    comment = f"{change.field} set to {change.new}"
+            return_data.append(
+                {
+                    "date": history.history_date,
+                    "user": history.history_user,
+                    "comment": comment,
+                }
+            )
+        return return_data
+
+    class Meta:
+        abstract = True
+
+
+class AbstractBaseModel(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=26,
+        default=generate_ulid,
+        unique=True,
+        blank=True,
+        editable=False,
+    )
+    created_at = models.DateTimeField("Created at", auto_now_add=True)
+    updated_at = models.DateTimeField("Updated at", auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class ImmutableBaseModel(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=26,
+        default=generate_ulid,
+        unique=True,
+        blank=True,
+        editable=False,
+    )
+    created_at = models.DateTimeField("Created at", auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,77 @@
+# ––– DJANGO IMPORTS
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from django.db.models.signals import post_delete, post_save, pre_save
+from django.dispatch import receiver, Signal
 
-# Create your models here.
+
+# ––– PYTHON UTILITY IMPORTS
+
+
+# –––THIRD-PARTY IMPORTS
+
+
+# ––– PROJECT IMPORTS
+from apps.core import models as core_models
+
+
+# ––– PARAMETERS
+
+
+# ––– MODELS
+
+
+class User(PermissionsMixin, AbstractBaseUser, core_models.AbstractBaseModel):
+    username_validator = UnicodeUsernameValidator()
+
+    USERNAME_FIELD = "username"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
+
+    email = models.EmailField("Email address", blank=True)
+    is_active = models.BooleanField("Active", default=True)
+    is_staff = models.BooleanField(
+        "staff status",
+        default=False,
+        help_text="Designates whether the user can log into this admin site.",
+    )
+    first_name = models.CharField(max_length=40, null=True, blank=True)
+    last_name = models.CharField(max_length=80, null=True, blank=True)
+    username = models.CharField(
+        "Username", max_length=255, unique=True, validators=[username_validator]
+    )
+
+    objects = UserManager()
+
+    class Meta:
+        ordering = [
+            "last_name",
+            "first_name",
+        ]
+
+    def __str__(self):
+        return f"{self.username}"
+
+
+class UserProfile(core_models.AbstractBaseModel):
+
+    avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+
+    def __str__(self):
+        return f"{self.user.username} | profile"
+
+
+@receiver(post_save, sender=User)
+def create_profile_on_user_creation(sender, created, instance, **kwargs):
+    """
+    Create UserProfile upon User instance creation
+    """
+    from apps.users.models import UserProfile
+
+    if created:
+        UserProfile(user=instance)
